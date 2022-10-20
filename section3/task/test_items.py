a9234b00d0735d9391b250c9fe072ffd6e0ddf8a
aleksandrvasilyev <saddon11@gmail.com>
2022-10-20 23:19:11 +0300

    task

---
+++ section3/task/test_items.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_see_add_to_cart_button(browser):
+    browser.get(link)
+    button = browser.find_elements_by_class_name("btn-add-to-basket")
+    time.sleep(5)
+    assert len(button) > 0, 'не найдено'
